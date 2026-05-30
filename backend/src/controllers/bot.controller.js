dcf0af3a560578fad323bf1ccb107bebee8bc257
itsMilanCodesu <milantamang350359@gmail.com>
2026-05-30 13:31:52 +0900

    feat : added api to update bot widget

--- backend/src/controllers/bot.controller.js
+++ backend/src/controllers/bot.controller.js
@@ -1,7 +1,9 @@
 
+import { set } from "mongoose"
 import botModel from "../models/bot.model.js"
 import {createBotService} from "../services/bot.service.js"
 import uploadImageService from "../services/uploadFile.service.js"
+import conversationModel from "../models/conversation.model.js"
 
 const createBotController = async (req, res , next) => {
   try {
@@ -68,9 +70,64 @@ console.log("inside get bot")
     }
   }
 
+  const updateBotSettingsController = async(req , res, next)=>{
+    try {
+
+      const businessId = req.user
+      const settings = req.body
+
+      const bot = await botModel.findOne({ businessId })
+      if(!bot){
+        return res.status(404).json({
+          message : "bot not found",
+          success : false
+        })
+      }
+      
+
+      console.log(settings)
+
+      const widgetSettings = bot.widgetSettings
+
+      const newWidgetSettings = {
+          primaryColor : settings?.primaryColor || widgetSettings.primaryColor,
+          botAvatar : settings?.botAvatar || widgetSettings.botAvatar,
+          botName : settings?.botName || widgetSettings.botName,
+          position : settings?.position || widgetSettings.position,
+          borderRadius : settings?.borderRadius || widgetSettings.borderRadius,
+          textColor : settings?.textColor || widgetSettings.textColor ,
+          welcomeMessage : settings?.welcomeMessage || widgetSettings.welcomeMessage,
+          placeholder : settings?.placeholder || widgetSettings.placeholder, 
+          systemPrompt : settings?.systemPrompt || widgetSettings.systemPrompt
+      }
+
+      console.log(newWidgetSettings.botName)
+     
+
+     const updatedBot =  await botModel.updateOne({ businessId }, { $set: { widgetSettings: newWidgetSettings } })
+
+      
+
+
+      res.status(200).json({
+        message : "bot settings updated successfully",
+        success : true,
+        bot 
+      })
+      
+      
+    } catch (error) {
+
+      console.log(error)
+      next(error)
+      
+    }
+  }
+
 
 
   export {
     createBotController, 
-    getBotController
+    getBotController, 
+    updateBotSettingsController
   }
